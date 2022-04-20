e2eaa93b359b5f71cd6cff0c870833b729e0be0a
istrejo <istrejo2106@gmail.com>
2022-04-20 18:20:48 -0400

    Ajustes al menu & nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -77,6 +77,7 @@ const routes: Routes = [
     path: 'website',
     loadChildren: () =>
       import('./website/website.module').then((m) => m.WebsiteModule),
+    canLoad: [AuthGuard],
   },
 ];
 
