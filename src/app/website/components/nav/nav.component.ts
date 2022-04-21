8c7e5e7c86cd883681d6cb1ccb05ab92ef192d39
istrejo <istrejo2106@gmail.com>
2022-04-21 10:55:01 -0400

    Mejorando el Menu

--- src/app/website/components/nav/nav.component.ts
+++ src/app/website/components/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../../core/services/login.service';
+import { UserService } from '../../../core/services/user.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,9 +9,17 @@ import { LoginService } from '../../../core/services/login.service';
 })
 export class NavComponent implements OnInit {
   activeMenu: boolean = false;
-  constructor(private loginService: LoginService) {}
+  userName: string;
+  constructor(
+    private loginService: LoginService,
+    private userService: UserService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userService.getUserInfo().subscribe((res) => {
+      this.userName = res.name;
+    });
+  }
 
   toggleMenu() {
     this.activeMenu = !this.activeMenu;
