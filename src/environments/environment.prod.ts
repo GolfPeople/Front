4cb3a4f562e21d976d131795920afc3aa366edb9
istrejo <istrejo2106@gmail.com>
2022-05-11 18:06:05 -0400

    firebase config prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  golfpeopleAPI: 'https://www.api.app.golfpeople.com'
+  golfpeopleAPI: 'https://www.api.app.golfpeople.com',
+  firebaseConfig: {
+    apiKey: 'AIzaSyC0OsRsAPOyimEggpzSGd_rfm6N_Hfrz2w',
+    authDomain: 'golf-people.firebaseapp.com',
+    projectId: 'golf-people',
+    storageBucket: 'golf-people.appspot.com',
+    messagingSenderId: '248337356424',
+    appId: '1:248337356424:web:aab2d7e1bb1bc13494313d',
+  },
 };
