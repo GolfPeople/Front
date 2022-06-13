2092980631c87bb5fdcfe019c3aa9f7a574bfb61
istrejo <istrejo2106@gmail.com>
2022-06-13 09:33:12 -0400

    Solicitud de amigo en buscador

--- src/app/pages/tabs/pages/friends/components/friend/friend.component.ts
+++ src/app/pages/tabs/pages/friends/components/friend/friend.component.ts
@@ -15,6 +15,7 @@ export class FriendComponent implements OnInit {
   myId;
   following: boolean = false;
   avatarDefault: string = 'assets/img/default-avatar.png';
+  sentFriendRequest: boolean = false;
 
   constructor(private friendsSvc: FriendsService) {
     this.myId = localStorage.getItem('user_id');
@@ -24,7 +25,7 @@ export class FriendComponent implements OnInit {
     // if (this.friend) {
     //   this.following = true;
     // }
-
+    console.log(this.user);
     this.isFriend ? (this.following = true) : (this.following = false);
     if (this.user.hasOwnProperty('to')) {
       if (this.user.to.length) {
@@ -36,6 +37,56 @@ export class FriendComponent implements OnInit {
         });
       }
     }
+
+    if (this.user.friends.length) {
+      console.log('Frends test -->');
+      this.user.friends.forEach((friendsItem) => {
+        if (friendsItem.connect.length) {
+          console.log('Connect test -->');
+          const friend = friendsItem;
+          // console.log(friend)
+          friendsItem.connect.forEach((connectItem) => {
+            console.log('Connect forEach test -->');
+            // console.log('connect item test -->', connectItem)
+
+            if (connectItem.user_id == this.myId) {
+              console.log('tienes conexion');
+              // if (friend.connections) {
+              if (friend.connections.status === 1) {
+                this.sentFriendRequest = true;
+                console.log('Ya has enviado una solicitud e amistad.');
+              } else if (friend.connections.status === 2) {
+                this.following = true;
+                console.log('Solicitud de amistad aprobada');
+              } else {
+                this.following = false;
+              }
+              // } else {
+              //   this.following = false;
+              // }
+            }
+          });
+        }
+
+        // if (item.connect.connection_id === 1) {
+        //     this.following = true;
+        //   console.log('Eres amigo');
+        // }
+
+        // if (item.user_id == this.myId) {
+        //   this.following = true;
+        //   console.log('Eres amigo');
+        // }
+      });
+    }
+  }
+
+  friendRequest() {
+    this.sentFriendRequest = true;
+    this.friendsSvc.friendRequest(this.user.id).subscribe((res) => {
+      console.log('Id del usuario a enviar solicitud', this.user.id);
+      console.log('Solicitud enviada -->', res);
+    });
   }
 
   follow(id) {
