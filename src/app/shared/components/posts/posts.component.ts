6480b86665dd0122c1f21937fbd5a8beaa0bb3c7
istrejo <istrejo2106@gmail.com>
2022-06-21 16:59:46 -0400

    scroll automatico

--- src/app/shared/components/posts/posts.component.ts
+++ src/app/shared/components/posts/posts.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  HostListener,
+  Inject,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { PostsResponse } from 'src/app/core/interfaces/interfaces';
 
 @Component({
@@ -7,19 +18,84 @@ import { PostsResponse } from 'src/app/core/interfaces/interfaces';
   styleUrls: ['./posts.component.scss'],
 })
 export class PostsComponent implements OnInit {
+  @ViewChild('list') private list: ElementRef;
   @Input() posts: PostsResponse[];
   @Input() loadingMore: boolean = false;
   @Output() loadMore = new EventEmitter();
 
-  constructor() {}
+  private hideScrollHeight = 200;
+  private showScrollheight = 500;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
   ngOnInit() {}
 
-  onLoadMore() {
+  onLoadMore(event) {
+    console.log('cargando más...');
     this.loadMore.emit();
+    // if (!this.loadingMore) {
+    //   event.target.disableb = true;
+    // }
+
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      // if (!this.loadingMore) {
+      //   event.target.disabled = true;
+      // }
+    }, 1500);
   }
 
-  onDelete(e, i){
-    this.posts.splice(i,1)
+  onDelete(e, i) {
+    this.posts.splice(i, 1);
   }
+
+  scrollBottom(): void {
+    try {
+      this.list.nativeElement.scrollTop = this.list.nativeElement.scrollHeight;
+      console.log('Estás haciendo scroll');
+    } catch (err) {}
+  }
+
+  // @HostListener('window: scroll', [])
+  // onWindowScroll(): void {
+  //   const yOffSet = window.pageYOffset;
+  //   if (
+  //     (yOffSet ||
+  //       this.document.documentElement.scrollTop ||
+  //       this.document.body.scrollTop) > this.showScrollheight
+  //   ) {
+  //     console.log('Se hizo el scroll');
+  //     this.loadingMore = true;
+  //   } else if (
+  //     (yOffSet ||
+  //       this.document.documentElement.scrollTop ||
+  //       this.document.body.scrollTop) < this.showScrollheight
+  //   ) {
+  //     this.loadingMore = false;
+  //   }
+  // }
+
+  // @HostListener('window: scroll', [])
+  // onWindowScroll(): void {
+  //   if (
+  //     window.innerHeight + window.scrollY >= this.document.body.offsetHeight &&
+  //     !this.loadingMore
+  //   ) {
+  //     console.log('bottom of the page');
+  //     this.loadingMore = true;
+  //     this.onLoadMore();
+  //   }
+  // }
+
+  // onScrollDown(): void {
+  //   this.onLoadMore();
+  //   // if (this.info.next) {
+  //   //   this.pageNum++;
+  //   //   this.getDataFromService();
+  //   // }
+  // }
 }
